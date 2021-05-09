617cb4b14b748def947d5759f5d745a79997f0ee
Aditya Goel <63554497+goeladityx@users.noreply.github.com>
2021-05-09 10:29:33 +0530

    Update functions.py

--- functions.py
+++ functions.py
@@ -24,7 +24,6 @@ def final_translate_for_eng(dataset, model, tokenizer, helsinki_model, helsinki_
             translated_sent.append(decoded_10)
         else:
             translated_sent.append(decoded_1)
-        print(i)
     return translated_sent
 
 def final_translate_for_other_languages(dataset, model, tokenizer, helsinki_model, helsinki_tokenizer):
@@ -47,7 +46,6 @@ def final_translate_for_other_languages(dataset, model, tokenizer, helsinki_mode
             translated_sent.append(decoded_10)
         else:
             translated_sent.append(decoded_1)
-        print(i)
     return translated_sent
 
 def load_model_other_than_english(language):  
