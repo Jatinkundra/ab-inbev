d5beb1ee814d1bcae1f43c1440f86215bc7a7b4b
Aditya Goel <63554497+goeladityx@users.noreply.github.com>
2021-05-10 07:40:25 +0530

    replaced processing.py

--- processing.py
+++ processing.py
@@ -1,5 +1,22 @@
 import json
 import requests
+
+def reload():
+  username = 'jatin26'
+  api_token = 'a62d062e9bb283a15de58a297bc0840e6e009807'
+  domain_name = "jatin26.pythonanywhere.com"
+
+  response = requests.post(
+      'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/'.format(
+          username=username, domain_name=domain_name
+      ),
+      headers={'Authorization': 'Token {token}'.format(token=api_token)}
+  )
+  if response.status_code == 200:
+      print('reloaded OK')
+  else:
+      print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+
 heroku_url = 'http://jatin26.pythonanywhere.com/' # change to your app name
 # sample data
 x= pd.read_json(input("File Path for dataFrame: "))
@@ -12,6 +29,7 @@ for count in range(int(len(x)/10)):
   y=x[a:b]
   y= y.to_dict()
   data = json.dumps(y)
+  reload()
   send_request.append(requests.post(heroku_url, data))
   if a==0:
     translations= send_request[0].json()["results"]
