731d4c97ec06ee38ecdaa31abd1be40d28be65e7
Jatinkundra <48369189+Jatinkundra@users.noreply.github.com>
2021-05-10 09:06:47 +0530

    Update processing.py

--- processing.py
+++ processing.py
@@ -1,34 +1,25 @@
 import json
 import requests
-
-def reload():
-  username = 'jatin26'
-  api_token = 'a62d062e9bb283a15de58a297bc0840e6e009807'
-  domain_name = "jatin26.pythonanywhere.com"
-
-  response = requests.post(
-      'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/'.format(
-          username=username, domain_name=domain_name
-      ),
-      headers={'Authorization': 'Token {token}'.format(token=api_token)}
-  )
-  if response.status_code == 200:
-      print('reloaded OK')
-  else:
-      print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
-
+# from LanguageIdentifier import predict
 heroku_url = 'http://jatin26.pythonanywhere.com/' # change to your app name
 # sample data
-x= pd.read_json(input("File Path for dataFrame: "))
-dataset_copy= list(x)
-#x= dataset_copy[0:30]
+x= pd.read_excel(input("File Path for dataFrame: "))
+x=x.to_dict()
+dataset_copy=[]
+for i in x.values():
+  a=i
+for i in a.values():
+  dataset_copy.append(i)
+
+# x= dataset_copy["French"][0:20]
 a=0
 b=10
 send_request=[]
-for count in range(int(len(x)/10)):
-  y=x[a:b]
+for count in range(int(len(dataset_copy)/10)):
+  y=dataset_copy[a:b]
+  y= pd.DataFrame(y)
   y= y.to_dict()
-  data = json.dumps(y)
+  data = json.dumps(y[0])
   reload()
   send_request.append(requests.post(heroku_url, data))
   if a==0:
