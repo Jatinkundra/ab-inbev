a973686d45695c6813b9e6f121e3c7128024b3f2
Aditya Goel <63554497+goeladityx@users.noreply.github.com>
2021-05-21 21:47:08 +0530

    added a faster translation for lang-english

--- functions.py
+++ functions.py
@@ -162,21 +162,23 @@ def translate_all_languages(language_1, language_2, dataset):
         print("english sentences done\n\n\n\n")
         # j=0
         lang=language_2
-        
-        translations_list=[]
-        try:
-            model and tokenizer
-        except NameError:
-            model, tokenizer= load_model_english_2(lang)
-        
-        try:
-            helsinki_model and helsinki_tokenizer
-        except NameError:
-            helsinki_tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-"+ lang)
-            helsinki_model =AutoModelWithLMHead.from_pretrained("/home/jatin26/ab-inbev/helsinki/opus-mt-en-"+lang, force_download=True)
-            
-        translations_list= final_translate_for_other_languages(lang, english_translate, model, tokenizer, helsinki_model, helsinki_tokenizer)
-        translated_dictionary[lang]= translations_list
+        if lang=="en":
+            translated_dictionary["en"]= english_translate
+        else:
+            translations_list=[]
+            try:
+                model and tokenizer
+            except NameError:
+                model, tokenizer= load_model_english_2(lang)
+
+            try:
+                helsinki_model and helsinki_tokenizer
+            except NameError:
+                helsinki_tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-"+ lang)
+                helsinki_model =AutoModelWithLMHead.from_pretrained("/home/jatin26/ab-inbev/helsinki/opus-mt-en-"+lang, force_download=True)
+
+            translations_list= final_translate_for_other_languages(lang, english_translate, model, tokenizer, helsinki_model, helsinki_tokenizer)
+            translated_dictionary[lang]= translations_list
                 # j=j+1
         return translated_dictionary
         # translated={}
