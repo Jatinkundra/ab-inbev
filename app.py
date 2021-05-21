cf41aec19eb790da551ea5b7d98e0f292b7a7954
Aditya Goel <63554497+goeladityx@users.noreply.github.com>
2021-05-21 22:28:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def predict():
     data_dict= translate_all_languages(language, dataset[1], dataset[:-1])
 
     file = open("output.txt", "a")
-    file.write(data_dict[dataset[1]])
+    file.write(data_dict[dataset[1]][0])
     file.close()
     # return data
 #     return jsonify(results= data_dict)
