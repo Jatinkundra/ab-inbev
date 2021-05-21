de364a44732e40fe894f29a4f470457df08ecdf5
Aditya Goel <63554497+goeladityx@users.noreply.github.com>
2021-05-21 10:47:29 +0530

    functions allignment changed

--- spelling_checker.py
+++ spelling_checker.py
@@ -1,15 +1,6 @@
 from spellchecker import SpellChecker
 from autocorrect import Speller
 
-def spell_model_activate(lng, sentence):
-    if lng=="it" or lng=="en":
-        spell=Speller(lang=lng)
-	senten= spell_check_italian(sentence,spell)
-    elif lng=="de" or lng=="fr":
-        spell=SpellChecker(language=lng)
-	senten= check_spell_en_fr_de(sentence,spell)
-    return senten
-
 def check_spell_en_fr_de(sentence,spell):
     #lang= input("Enter the language")
     #sentence=input("Enter the sentence")
@@ -28,4 +19,13 @@ def spell_check_italian(sentence,spell):
     for word in sentence.split():
         word= spell(word)
         senten= senten+word+" "
+    return senten
+
+def spell_model_activate(lng, sentence):
+    if lng=="it" or lng=="en":
+        spell=Speller(lang=lng)
+	senten= spell_check_italian(sentence,spell)
+    elif lng=="de" or lng=="fr":
+        spell=SpellChecker(language=lng)
+	senten= check_spell_en_fr_de(sentence,spell)
     return senten
